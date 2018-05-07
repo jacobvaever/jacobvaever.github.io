f4ed518e9a867790239d821b4242dc30f966ce27
jacobvaever <jacobvaever@hotmail.com>
2018-05-07 13:41:20 +0200

    database test

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 // main code script
-$(document).ready(function(){
-    $("#save").click(function(){
-        var x = $("#input11").val();
-    });
+     // hold op!
+// $(document).ready(function(){
+//     $("#save").click(function(){
+//         var x = $("#input11").val();
+//     });
+// });
+
+$(function(){
+    function saveToiletParameters(name) {
+        firebase.database().ref().set({
+            username: name
+        });
+        
+    }
+   
 });
+
+saveToiletParameters(name);
 
