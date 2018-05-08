77103d824dd7119713e228da0da2e0b0df9e33ba
jacobvaever <jacobvaever@hotmail.com>
2018-05-08 14:11:30 +0200

    jacob:  in progress

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 // main code script
      // hold op!
-function writeToiletParameters(name) {
-    firebase.database().ref('name-base').set({
-        username: name
-    });        
+function writeFormToDatabase(form) {
+    var i, jobArray,jobName;
+    var
+    for (j in $("div.container-fluid")){
+        for (i = 0; i < $('table >tbody >tr').length; i++){
+            jobArray = JSON.stringify(
+                {form.job-per.value,
+                                        form.job-mas.value,
+                                        form.job-tid.value]);
+            jobName = 
+        }
+    }
+    // firebase.database().ref('job-base').set({
+    //     : name
+    // });        
 }
 // function  readToiletParameters(){
 //     firebase.database().ref('name-base').on()
 // }
 
+
+
+function giveittome(form) {
+    var newName = form.name.value;
+    alert('your name is: ');
+}
+
 $(document).ready(function(){
-    $("button").click(function(){
-        writeToiletParameters($("#name").val());
-        $
+    $("#row").click(function(){
+        var rows = $('table >tbody >tr').length;
+        
     });
 });
 
